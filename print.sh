0fd889ad8ad3c2892a6b4d53b498b3dc163450cd
ngowrish <gnavien@gmail.com>
2023-06-23 18:30:00 +0530

    color change

--- print.sh
+++ print.sh
@@ -2,3 +2,7 @@
 echo Hello World
 
 echo -e "\e[31mHello in Red color\e[0m"
+echo -e "\e[32mHello in Green color\e[0m"
+echo -e "\e[33mHello in Yellow color\e[0m"
+echo -e "\e[34mHello in Blue color\e[0m"
+echo -e "\e[35mHello in Mahenta color\e[0m"
